2559029907c99e41662fb1685326778ac3a61623
GitButtowski <chaudharyansh0006@gmail.com>
2026-06-06 12:55:44 +0530

    Created API for admin to look into revisions

---
+++ src/main/java/com/project/prodReadyFeatures/Controllers/AuditController.java
@@ -0,0 +1,33 @@
+package com.project.prodReadyFeatures.Controllers;
+
+import com.project.prodReadyFeatures.Entities.PostEntity;
+import jakarta.persistence.EntityManagerFactory;
+import org.hibernate.envers.AuditReader;
+import org.hibernate.envers.AuditReaderFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(path = "/audit")
+public class AuditController {
+
+    @Autowired
+    private EntityManagerFactory entityManagerFactory;
+
+    @GetMapping(path = "/posts/{postId}")
+    List<PostEntity> getPostRevisions(@PathVariable Long postId){
+
+        AuditReader reader = AuditReaderFactory.get(entityManagerFactory.createEntityManager());
+
+        List<Number> revisions = reader.getRevisions(PostEntity.class, postId);
+
+        return revisions.stream()
+                .map(revisionNumber -> reader.find(PostEntity.class, postId, revisionNumber))
+                .toList();
+
+    }
+
+}
